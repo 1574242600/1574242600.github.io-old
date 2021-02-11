02dc56758951d9af25f2c02db1574948e83eb12c
Nworm <1574242600@qq.com>
2021-02-11 16:32:42 +0800

    update: 更新主题至0.0.7 新增rss

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,5 +23,61 @@ module.exports = {
     },
     plugins: [
         { resolve: "gatsby-theme-yoshino", options: {}},
+        {
+            resolve: "gatsby-plugin-feed",
+            options: {
+                query: `
+                    {
+                        site {
+                            siteMetadata {
+                                url
+                                title
+                                description
+                            }
+                        }
+                    }
+                `,
+                feeds: [
+                    {
+                        serialize: ({ query: { site, allMarkdownRemark } }) => {
+                            return allMarkdownRemark.edges.map(edge => {
+                                return Object.assign({}, edge.node.frontmatter, {
+                                    description: edge.node.excerpt,
+                                    date: edge.node.frontmatter.date,
+                                    url: site.siteMetadata.url + edge.node.fields.slug,
+                                    guid: site.siteMetadata.url + edge.node.fields.slug,
+                                    custom_elements: [{ "content:encoded": edge.node.html }],
+                                });
+                            });
+                        },
+                        query: `
+                            {
+                                allMarkdownRemark(
+                                    sort: { order: DESC, fields: [frontmatter___date] },
+                                    filter: {fileAbsolutePath: {regex: "/(?<!about)\\\\.md$/"}}
+                                ) {
+                                    edges {
+                                        node {
+                                            excerpt
+                                            html
+                                            fields { 
+                                                slug 
+                                            }
+                                            frontmatter {
+                                                title
+                                                date
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        `,
+                        output: "/rss.xml",
+                        title: "Nworm",
+                        match: "^/post/",
+                    },
+                ],
+            },
+        }
     ],
 };
